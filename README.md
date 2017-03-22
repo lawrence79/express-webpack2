580ccc3f5a009df724747414dd4d17187dd29f4d
Lawrence Norton <lawrence@lawrencenorton.com>
2017-03-22 12:46:25 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# AARP Skills Decoder
+
+## Pull CV from LinkedIn
+
+## Pull CV from CSV
+
+## Pull CV from Word/Pdf/Webpage
---
+++ test/test.js
@@ -0,0 +1,13 @@
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it.only('should return -1 unless present', function() {
+      // this test will be run
+    });
+
+    it.only('should return the index when present', function() {
+      // this test will also be run
+    });
+
+    it('should return -1 if called with a non-Array context')
+  });
+});
