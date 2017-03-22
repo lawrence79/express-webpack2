beb0db0b281ab64ff1ae902bf2fa411e4223973f
Lawrence Norton <lawrence@lawrencenorton.com>
2017-03-22 14:25:21 -0400

    updated

--- test/test.js
+++ test/test.js
@@ -1,13 +1,17 @@
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it.only('should return -1 unless present', function() {
-      // this test will be run
-    });
+var chai = require('chai');
+var textract = require('textract');
+var filePath = '../resumes/resume-1.docx';
+var docType =  'appLication/vnd.openXMLformats-Officedocument.WordProcessingml.Document';
 
-    it.only('should return the index when present', function() {
-      // this test will also be run
-    });
-
-    it('should return -1 if called with a non-Array context')
-  });
+describe('Skills Decoder', function() {
+    it('should parse a text file')
+    it('should parse a pdf file')
+    it('should parse a .doc or .docx file', function() {
+      textract.fromFileWithPath(filePath, function( error, text ) {
+        console.log('Error', error);
+        console.log('Text', text);
+        // do some checks with the data
+      })
+    })
+    it('should parse a csv file')
 });
